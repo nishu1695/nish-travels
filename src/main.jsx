5cdb5eb69312c95d593ad63aeff53401f941c090
Nisarg Shah <nisargshah16@hotmail.com>
2026-06-10 14:20:01 -0400

    updated router to Hashbrowser

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import "leaflet/dist/leaflet.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/nish-travels/">
+  <HashRouter basename="/nish-travels/">
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
